4eef40be2d1380e643c1009df52afb6089b3ba0f
preeti@67 <preeti67@gmail.com>
2025-04-01 03:00:43 +0530

    serverjs modified with cors

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(logger);
 app.use(express.json());
-
+app.use(cors({origin: "http://localhost:5173"}));
 app.use("/api/smv1", userRoutes);
 
 const PORT = process.env.PORT || 4012;
